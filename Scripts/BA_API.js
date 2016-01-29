411bec9592ae4aaad412bb1f94a2e1f289f8697e
MrMoko <joshb98@comcast.net>
2016-01-29 17:53:56 -0500

    Able to get 1 page at a time

---
+++ Scripts/BA_API.js
@@ -0,0 +1,20 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "www.thebluealliance.com/api/v2/teams/;
+
+xmlhttp.onreadystatechange = function() {
+	if (xmkhttp.readyState == 4 && xmlhhtp.status == 200) {
+		var page = JSON.parase(xmlhttp.responseText); 
+	}
+};
+function funk(arr) {
+
+xmlhttp.open("GET", www.thebluealliance.com/api/v2/teams/, true);
+xmlhttp.send();
+;
+	x = 0
+	var out = "";
+	var i, x
+	for(i = 0; i < 12; i++) {
+	x += 1
+	}
+}
--- Scripts/find.js
+++ Scripts/find.js
@@ -1,4 +1,28 @@
-var x = 5, y = 10, z = 20;
+function req(){
+for(var i = 1; i < 13;){
+var xmlhttp = new XMLHttpRequest();
+var url = "http://thebluealliance.com/api/v2/teams/" + i + '?X-TBA-App-Id=frc2729:scouting-system:v01';
 
-window.alert(x + y + z);
-document.getElementById('blank').innerHTML = '[Team Numbers Go Here]';
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var myArr = JSON.parse(xmlhttp.responseText);
+        numFind(myArr);
+    }
+i += 1
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+}
+}
+/*
+function numFinder(arr){
+	var out = arr[0].team_number + arr[1].team_number.toString() + arr[2].team_number.toString() + arr[3].team_number.toString() + arr[4].team_number.toString() + arr[5].team_number.toString() + arr[6].team_number.toString() + arr[7].team_number.toString() + arr[8].team_number.toString() + arr[9].team_number.toString() + arr[10].team_number.toString() + arr[11].team_number.toString() + arr[13].team_number.toString();
+	document.getElementById("blank").innerHTML = out;
+}
+*/
+function numFind(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; ++i) {out += arr[i].team_number + "<br>";console.log(out);}
+    document.getElementById("blank").innerHTML = out;
+}
