2e3795eea97a8df4bebadc606d37e8cdefc17c30
MrMoko <joshb98@comcast.net>
2016-02-16 17:40:49 -0500

    New Android Studio Project

---
+++ TeamNumberFinderApp/app/src/main/java/stormroboticsteam/teamnumberfinderapp/MainActivity.java
@@ -0,0 +1,22 @@
+package stormroboticsteam.teamnumberfinderapp;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public void changeBlank(View view) {
+        ((TextView) findViewById(R.id.blank)).setText("Say Yes " + 3+5 );
+    }
+}
