2d7f057c6364d964593ed6961432aef6751eaf47
MrMoko <joshb98@comcast.net>
2016-02-23 17:27:50 -0500

    Hoi

--- TeamNumberFinderApp/app/src/main/java/stormroboticsteam/teamnumberfinderapp/MainActivity.java
+++ TeamNumberFinderApp/app/src/main/java/stormroboticsteam/teamnumberfinderapp/MainActivity.java
@@ -78,3 +78,29 @@ public class MainActivity extends AppCompatActivity {
             return resultToDisplay;
         }
     }
+    class HttpRequestTask extends AsyncTask<Void, Void, Greeting> {
+        @Override
+        protected Greeting doInBackground(Void... params) {
+            try {
+                final String url = "http://rest-service.guides.spring.io/greeting";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+                return greeting;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+           return null;
+    }
+
+        @Override
+        protected void onPostExecute(Greeting greeting) {
+            TextView greetingIdText = (TextView) findViewById(R.id.id_value);
+            TextView greetingContentText = (TextView) findViewById(R.id.content_value);
+            greetingIdText.setText(greeting.getId());
+            greetingContentText.setText(greeting.getContent());
+        }
+
+    }
+
